80874fef6d6d2392d775bfcd86327ac666ce44b4
Alex Petty <pettyalex@gmail.com>
2021-05-18 22:28:28 -0500

    Generate project in spring boot initializr

---
+++ src/test/kotlin/net/catjunior/awfulparser/AwfulparserApplicationTests.kt
@@ -0,0 +1,13 @@
+package net.catjunior.awfulparser
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AwfulparserApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
